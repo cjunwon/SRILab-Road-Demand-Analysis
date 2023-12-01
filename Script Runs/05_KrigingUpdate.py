cfe460a41723c7c46bf936371263b017a4d64b60
Junwon Choi <cjunwon.tech@gmail.com>
2023-12-01 15:09:01 -0800

    Kriging visualization update

--- Script Runs/05_KrigingUpdate.py
+++ Script Runs/05_KrigingUpdate.py
@@ -15,86 +15,68 @@ udf_before = udf.copy()
 # print(udf['Street_Designation'].unique())
 # print(len(udf['Street_Designation'].unique()))
 
+print('Potential Designations: \n')
 for designation in udf['Street_Designation'].unique():
     print(designation)
 
-# S000_adjusted
-
-for designation in udf['Street_Designation'].unique():
-    print(designation)
-    x1 = udf[(udf['Street_Designation'] == designation) & (udf['S000_adjusted'] != 0)]['centroid_lat'].to_numpy()
-    y1 = udf[(udf['Street_Designation'] == designation) & (udf['S000_adjusted'] != 0)]['centroid_lon'].to_numpy()
-    z_S000 = udf[(udf['Street_Designation'] == designation) & (udf['S000_adjusted'] != 0)]['S000_adjusted'].to_numpy()
-
-    x2 = udf[(udf['Street_Designation'] == designation) & (udf['S000_adjusted'] == 0)]['centroid_lat'].to_numpy()
-    y2 = udf[(udf['Street_Designation'] == designation) & (udf['S000_adjusted'] == 0)]['centroid_lon'].to_numpy()
-
-    if len(x1) == 0:
-        print("No data for designation: ", designation)
-
-    else:
-        if len(np.unique(z_S000)) == 1:
-            print("All values in z1 are constant for designation: ", designation)
-        else:
-            print("Values in z1 are not constant.")
-            variogram_model_S000 = Variogram(np.vstack((x1, y1)).T, z_S000)
-
-            ok_S000 = OrdinaryKriging(variogram_model_S000, min_points=2, max_points=10, mode='exact')
-
-            field_S000 = ok_S000.transform(x2, y2)
-
-            print(str(len(field_S000)) + " values in field for designation: ", designation)
-            print(str(len(x2)) + " should ideally be updated for designation: ", designation)
-
-            # norm = Normalize(vmin=min(np.min(z1), np.min(field)), vmax=max(np.max(z1), np.max(field)))
-
-            if len(x2) == len(field_S000):
-                print("Lengths match for designation: ", designation)
-                for i in range(len(x2)):
-                    udf.loc[(udf['centroid_lat'] == x2[i]) & (udf['centroid_lon'] == y2[i]), 'S000_adjusted'] = field_S000[i]
-
-            else:
-                print("Lengths do not match for designation: ", designation)
-                # for i in range(len(x2)):
-                #     udf.loc[(udf['centroid_lat'] == x2[i]) & (udf['centroid_lon'] == y2[i]), 'S000_adjusted'] = field_S000[i]
-
-print("Before Kriging - number of rows with 0 in S000_adjusted: ", len(udf_before[(udf_before['S000_adjusted'] == 0)]))
-print("After Kriging - number of rows with 0 in S000_adjusted: ", len(udf[(udf['S000_adjusted'] == 0)]))
-
-# All other adjusted columns
-
-adj_cols = ['SA01_adjusted', 'SA02_adjusted', 'SA03_adjusted', 'SE01_adjusted', 'SE02_adjusted', 'SE03_adjusted', 'SI01_adjusted', 'SI02_adjusted', 'SI03_adjusted']
+adj_cols = ['SA01_adjusted', 'SA02_adjusted', 'SA03_adjusted', 'SE01_adjusted', 'SE02_adjusted', 'SE03_adjusted', 'SI01_adjusted', 'SI02_adjusted', 'SI03_adjusted', 'S000_adjusted']
 
 for col in adj_cols:
-    print("Currently updating: ", col)
+    print("\n Currently updating: ", col)
     for designation in udf['Street_Designation'].unique():
         print(designation)
-        x1 = udf[(udf['Street_Designation'] == designation) & (udf[col] != 0)]['centroid_lat'].to_numpy()
-        y1 = udf[(udf['Street_Designation'] == designation) & (udf[col] != 0)]['centroid_lon'].to_numpy()
-        z1 = udf[(udf['Street_Designation'] == designation) & (udf[col] != 0)][col].to_numpy()
+        # Set x1, y1, z1 for existing points
+        x1 = udf[(udf['Street_Designation'] == designation) & (udf['S000_adjusted'] != 0)]['centroid_lat'].to_numpy()
+        y1 = udf[(udf['Street_Designation'] == designation) & (udf['S000_adjusted'] != 0)]['centroid_lon'].to_numpy()
+        z1 = udf[(udf['Street_Designation'] == designation) & (udf['S000_adjusted'] != 0)][col].to_numpy()
 
-        x2 = udf[(udf['Street_Designation'] == designation) & (udf[col] == 0)]['centroid_lat'].to_numpy()
-        y2 = udf[(udf['Street_Designation'] == designation) & (udf[col] == 0)]['centroid_lon'].to_numpy()
+        # Set x2, y2 for points to be updated (with S000_adjusted == 0)
+        x2 = udf[(udf['Street_Designation'] == designation) & (udf['S000_adjusted'] == 0)]['centroid_lat'].to_numpy()
+        y2 = udf[(udf['Street_Designation'] == designation) & (udf['S000_adjusted'] == 0)]['centroid_lon'].to_numpy()
 
+        # Error handling
         if len(x1) == 0:
-            print("No data for designation: ", designation)
+            print("\n No data for designation: ", designation)
+
+        if len(z1) == 0:
+            break
 
         else:
+            # Error handling
             if len(np.unique(z1)) == 1:
-                print("All values in z1 are constant for designation: ", designation)
+                print("\n All values in z1 are constant for designation: ", designation)
             else:
-                print("Values in z1 are not constant.")
+                # We want z1 to be not constant for the variogram model to be created
+                print("\n Values in z1 are not constant.")
                 variogram_model = Variogram(np.vstack((x1, y1)).T, z1)
 
+                # Apply Kriging to get field values for x2, y2
                 ok = OrdinaryKriging(variogram_model, min_points=2, max_points=10, mode='exact')
 
                 field = ok.transform(x2, y2)
 
                 print(str(len(field)) + " values in field for designation: ", designation)
                 print(str(len(x2)) + " should ideally be updated for designation: ", designation)
+                
+                # Plotting Kriging predictions
+                norm = Normalize(vmin=min(np.min(z1), np.min(field)), vmax=max(np.max(z1), np.max(field)))
+                fig_name = str(col) + "_" + str(designation)
 
-                # norm = Normalize(vmin=min(np.min(z1), np.min(field)), vmax=max(np.max(z1), np.max(field)))
+                plt.figure(fig_name, figsize=(20, 10))
 
+                scatter1 = plt.scatter(x1, y1, c=z1, norm=norm, marker='o', label='Existing Points')
+                scatter2 = plt.scatter(x2, y2, c=field, norm=norm, marker='x', s=200, label='Kriging Predictions')
+
+                sample_label = 'Z Values (Existing) - ' + str(fig_name)
+                kriging_label = 'Z Values (Kriging) - ' + str(fig_name)
+
+                cbar = plt.colorbar(scatter1, label=sample_label)
+                cbar2 = plt.colorbar(scatter2, label=kriging_label)
+                plt.legend()
+                plt.savefig("kriging_visuals/figure_" + str(fig_name) + ".png", format='png', dpi=200)
+                plt.show()
+
+                # Update udf with kriging predictions
                 if len(x2) == len(field):
                     print("Lengths match for designation: ", designation)
                     for i in range(len(x2)):
@@ -102,5 +84,8 @@ for col in adj_cols:
 
                 else:
                     print("Lengths do not match for designation: ", designation)
+
+print("Before Kriging - number of rows with 0 in S000_adjusted: ", len(udf_before[(udf_before['S000_adjusted'] == 0)]))
+print("After Kriging - number of rows with 0 in S000_adjusted: ", len(udf[(udf['S000_adjusted'] == 0)]))
 
 udf.to_file(r"hillside_inventory_LA_centrality_full_new_evacmidnorth_lodes_kriging.geojson", driver='GeoJSON')
